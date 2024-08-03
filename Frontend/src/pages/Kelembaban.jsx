22535fd86da3441e9339f045e6070a677338d23c
anggasputraaaaa <26anggasaputra@gmail.com>
2024-08-03 21:17:16 +0800

    update

--- Frontend/src/pages/Kelembaban.jsx
+++ Frontend/src/pages/Kelembaban.jsx
@@ -24,7 +24,7 @@ const Kelembaban = () => {
         
         // Extract the top 10 latest entries and map to desired format
         const newData = sortedData.slice(0, 15).map(item => ({
-          Kelembaban: item.humidity.toString(),
+          kelembaban: item.humidity.toString(),
           waktu: item.time,
           tanggal: item.date,
         }));
